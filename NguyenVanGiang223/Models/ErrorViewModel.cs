c212172adfc7e02484f05d358f0d3b2e4ef1a975
NguyenGiang14 <giangbnbn123@gmail.com>
2024-10-18 00:44:14 +0700

    Create_Project

---
+++ NguyenVanGiang223/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenVanGiang223.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
